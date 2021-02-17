02c6add9dcead5be6c0d74dc5e08f6df24ca71f6
Alexandre <ryxandy@hotmail.com>
2021-02-16 22:57:48 -0300

    basic implementation restTemplate

--- hr-payroll/src/main/java/com/microservice/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/microservice/hrpayroll/resources/PaymentResource.java
@@ -19,7 +19,7 @@ public class PaymentResource {
 	
 	@GetMapping(value = "/{workerId}/days/{days}")
 	public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days){
-		Payment payment = service.getPayment(workerId, days);
+		Payment payment = service.getPayment(workerId, days); 
 		return ResponseEntity.ok(payment);
 	}
 
